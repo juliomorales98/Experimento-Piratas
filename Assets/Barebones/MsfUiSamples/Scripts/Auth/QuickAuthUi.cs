61b2dc2e573193be370434b7238bd9613736253f
juliomorales98 <juliocesar.mr@protonmail.com>
2020-01-26 15:48:14 -0600

    Cliente se conecta con la ip que se le proporcione.
    Se le agregó al cliente externo La posibilidad de conectarse a una ip diferente a la local, haciendo así posible la conexión LAN.

---
+++ Assets/Barebones/MsfUiSamples/Scripts/Auth/QuickAuthUi.cs
@@ -0,0 +1,84 @@
+﻿using Barebones.MasterServer;
+using UnityEngine;
+using System.Collections.Generic;
+//using System.Diagnostics;
+using System.Threading;
+
+namespace Barebones.MasterServer
+{
+    public class QuickAuthUi : MonoBehaviour
+    {
+        public GameObject LoginWindow;
+        public GameObject RegisterWindow;
+
+        public bool DeactivateOnLogIn = true;
+
+        //public List<GameObject> DisableObjectsOnLogIn;
+
+        void Awake()
+        {
+            LoginWindow = LoginWindow ?? FindObjectOfType<LoginUi>().gameObject;
+            RegisterWindow = RegisterWindow ?? FindObjectOfType<RegisterUi>().gameObject;
+            Msf.Client.Auth.LoggedIn += OnLoggedIn;
+
+            // In case we're already logged in 
+            if (Msf.Client.Auth.IsLoggedIn)
+                OnLoggedIn();
+        }
+
+        private void OnLoggedIn()
+        {
+            if (DeactivateOnLogIn)
+                gameObject.SetActive(false);
+        }
+
+        public void OnLoginClick()
+        {
+            if (!Msf.Client.Auth.IsLoggedIn)
+                LoginWindow.gameObject.SetActive(true);
+        }
+
+        public void ConnectToServer(){
+            var ConnectionToMaster = FindObjectOfType<ConnectionToMaster>();
+            //ConnectionToMaster.Awake();
+            ConnectionToMaster.StartMio();
+            Debug.Log("Terminó Script");
+        }
+        public void OnGuestAccessClick()
+        {
+            /*var ConnectionToMaster = FindObjectOfType<ConnectionToMaster>();
+            //ConnectionToMaster.Awake();
+            ConnectionToMaster.StartMio();
+            Debug.Log("Terminó Script");*/
+
+            
+            
+            var promise = Msf.Events.FireWithPromise(Msf.EventNames.ShowLoading, "Logging in");
+            
+            Msf.Client.Auth.LogInAsGuest((accInfo, error) =>
+            {
+                promise.Finish();
+
+                if (accInfo == null)
+                {
+
+                    Msf.Events.Fire(Msf.EventNames.ShowDialogBox, DialogBoxData.CreateError(error));
+                    Logs.Error(error);
+                }/*else{
+                    //Si entramos a la conexión ponemos en null los objetos para poner ip
+                    foreach (var obj in DisableObjectsOnLogIn)
+                    {
+                        if (obj != null)
+                            obj.SetActive(false);
+                    }
+                }*/
+            });
+        }
+
+        public void OnRegisterClick()
+        {
+            if (!Msf.Client.Auth.IsLoggedIn)
+                RegisterWindow.SetActive(true);
+        }
+    }
+}
