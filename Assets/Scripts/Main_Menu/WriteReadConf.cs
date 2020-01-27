719e057612033e436855ba95ef4f048f0d570ba1
juliomorales98 <juliocesar.mr@protonmail.com>
2020-01-27 12:58:44 -0600

    Pruebas para escribir en disco.
    Se creo un archivo para pruebas y guardar/leer configuración del juego para futuro.

---
+++ Assets/Scripts/Main_Menu/WriteReadConf.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class WriteReadConf : MonoBehaviour {
+
+	private string path = "conf.txt";
+	public void WriteConf(){
+		StreamWriter writer = new StreamWriter(path, true);
+		writer.WriteLine("Puebas");
+		writer.Close();
+	}
+
+	void Start(){
+		WriteConf();
+	}
+}
