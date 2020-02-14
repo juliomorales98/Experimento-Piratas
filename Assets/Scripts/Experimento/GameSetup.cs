e1eb1fda6ba985959859caed26ff873fd8d901a9
juliomorales98 <juliocesar.mr@protonmail.com>
2020-02-13 18:53:08 -0600

    Se le agregan spawnpoints
    Se le agregan spawnpoints al juego, pero aún no incializa a los jugadores correctamente.

--- Assets/Scripts/Experimento/GameSetup.cs
+++ Assets/Scripts/Experimento/GameSetup.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 public class GameSetup : MonoBehaviour {
 
+	public static GameSetup GS;
+
+	public Transform[] spawnPoints;
+
 	[SerializeField]
 	private Text pirata;
 	// Use this for initialization
 
 	private bool instanciado = false;
+
+	private void OnEnable(){
+		if(GameSetup.GS == null){
+			GameSetup.GS = this;
+		}
+	}
 	void Start () {
 		//PhotonNetwork.AutomaticallySyncScene = true;//activamos de nuevo la sincronización de escenas.
 		Debug.Log("Entró a game setup");
@@ -24,28 +34,11 @@ public class GameSetup : MonoBehaviour {
 	
 	// Update is called once per frame
 	private void CreatePlayer(){
-		//PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs","PhotonPlayer1"),new Vector3(-1.3f,6.4f,9.8f), Quaternion.identity);
-		string photonPlayer = "";
-
-
-		switch(pirata.text){
-			case "Pirata 1":
-				photonPlayer = "PhotonPlayer1";
-				break;
-			case "Pirata 2":
-				photonPlayer = "PhotonPlayer2";
-				break;
-			case "Pirata 3":
-				photonPlayer = "PhotonPlayer3";
-				break;
-			case "Pirata 4":
-				photonPlayer = "PhotonPlayer4";
-				break;
-			default:
-				return;
-		}
+		PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs","PhotonNetworkPlayer"),new Vector3(-1.3f,6.4f,9.8f), Quaternion.identity);
 		
-		PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs",photonPlayer),new Vector3(-1.3f,6.4f,9.8f), Quaternion.identity);
-		Debug.Log(photonPlayer + " conectado");
+	}
+	
+	public Text getPirataName(){
+		return pirata;
 	}
 }
---
+++ Assets/Scripts/Experimento/PhotonPlayer.cs
@@ -0,0 +1,42 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class PhotonPlayer : MonoBehaviour {
+
+	private PhotonView PV;
+
+	public GameObject myAvatar;
+
+	private Text pirata;
+
+	private bool instanciado = false;
+	// Use this for initialization
+
+	void Start(){
+		pirata = GameObject.Find("Pirate Name").GetComponent<Text>();
+	}
+
+	void Update () {
+		if(pirata.text != "Nombre Pirata" && !instanciado){
+			CreateAvatar();
+			instanciado = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void CreateAvatar () {
+		PV = GetComponent<PhotonView>();
+		int spawnPicker = Random.Range(0,GameSetup.GS.spawnPoints.Length);
+		if(PV.IsMine){		
+		
+			 myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs",pirata.text),
+			GameSetup.GS.spawnPoints[spawnPicker].position,GameSetup.GS.spawnPoints[spawnPicker].rotation,
+			0);
+
+		}
+	}
+}
