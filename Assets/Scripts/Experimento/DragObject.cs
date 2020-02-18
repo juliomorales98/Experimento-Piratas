36a5bda8352d3e5747f19b1a93bc335f3940ee24
juliomorales98 <juliocesar.mr@protonmail.com>
2020-02-18 10:00:05 -0600

    Mejora en el drag.
    Añadimos script que accede a lo que el jugador clickea, haciendo que tengamos información del que está moviendo la pieza.

--- Assets/Scripts/Experimento/DragObject.cs
+++ Assets/Scripts/Experimento/DragObject.cs
@@ -9,44 +9,27 @@ public class DragObject : MonoBehaviour {
     private float mZCoord;
 
 	private Vector3 originalRotation;
-
-	private bool gettingDraged;
-
-	void Start(){
-		gettingDraged = false;
-	}
-
-	void Update(){
-
-	}
-    void OnMouseDown(){
-
-		gettingDraged = true;
-
-		Debug.Log(transform.rotation.x);
-		originalRotation = new Vector3(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y,transform.rotation.eulerAngles.z);
+    
+    public void MovePiece(){
+        originalRotation = new Vector3(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y,transform.rotation.eulerAngles.z);
 
         mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-        // Store offset = gameobject world pos - mouse world pos
+        
         mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
 
 		//Quitamos las rotaciones para que no se aloque
 		transform.eulerAngles = originalRotation;
     }
 
-	void OnMouseUp(){
-		gettingDraged = false;
-	}
-
     private Vector3 GetMouseAsWorldPoint(){
 
-        // Pixel coordinates of mouse (x,y)
+        
         Vector3 mousePoint = Input.mousePosition;
 
-        // z coordinate of game object on screen
+        
         mousePoint.z = mZCoord;
 
-        // Convert it to world points
+        // Convertimos coordenadas a posiciones en el world.
         return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 
---
+++ Assets/Scripts/Experimento/ObjectClicker.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClicker : MonoBehaviour {
+
+	public float force = 10;
+	private Vector3 mOffset;
+
+    private float mZCoord;
+
+	private Vector3 originalRotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButtonDown(0)){
+			RaycastHit hit;
+
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if(Physics.Raycast(ray,out hit, 100.0f)){
+				if(hit.transform){
+										
+					if(hit.transform.GetComponent<Rigidbody>()){
+						
+						MoveObject(hit);
+					}
+				}
+			}
+		}
+		
+	}
+
+	private void MoveObject(RaycastHit hit){
+		
+		hit.transform.GetComponent<DragObject>().MovePiece();
+		
+	}
+	private void PrintName(GameObject go){
+		Debug.Log(go.name);
+	}
+}
