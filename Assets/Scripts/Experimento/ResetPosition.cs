691cf87d85014210047ddb76b548081be819ec6a
Julio <juliocesar.mr@protonmail.com>
2020-03-04 14:24:05 -0600

    Agregamos el reset de la pieza.

---
+++ Assets/Scripts/Experimento/ResetPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour {
+
+	private Vector3 initialPosition;
+	// Use this for initialization
+	void Start () {
+		initialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y < -2f){
+			transform.position = initialPosition;
+			
+		}
+	}
+}
