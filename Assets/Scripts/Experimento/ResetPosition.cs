e942f2baed0f95abb97f33740dd0e1e9f29abbcc
juliomorales98 <juliocesar.mr@protonmail.com>
2020-03-12 16:47:44 -0600

    Trabajamos reset de pieza.
    Agregamos más validaciones de la pieza, falta quitarle la velocidad que trae cuando se le cambia la posición.

--- Assets/Scripts/Experimento/ResetPosition.cs
+++ Assets/Scripts/Experimento/ResetPosition.cs
@@ -12,7 +12,7 @@ public class ResetPosition : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(transform.position.y < -2f){
+		if(transform.position.y > 45 || transform.position.y < -2f || transform.position.z > 260f || transform.position.z < -200f || transform.position.x > 270f || transform.position.x < -200f){
 			transform.position = initialPosition;
 			
 		}
